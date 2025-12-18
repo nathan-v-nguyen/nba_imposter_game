5172eb4aa55c4808045f565144a313abb62450e2
Nathan Nguyen <nvnguyen109@gmail.com>
2025-12-18 13:58:23 -0800

    complete terminal game

--- imposter_game.py
+++ imposter_game.py
@@ -1,11 +1,28 @@
 import numpy as np
 import pandas as pd
 
+def get_num_players():
+  '''Returns number of players'''
+  while True:
+    try:
+      num_players = int(input("Enter the Nummber of Players: "))
+      if num_players < 3:
+        print('You need atleast 3 players to play')
+        continue
+      return num_players
+    except ValueError:
+        print('Please enter a valid integer')
+
 def collect_players(num_players: int) -> list[str]:
   '''Return list of players'''
   players = []
   for i in range(num_players):
-    player = input('Enter Player Name: ')
+    while True:
+      player = input('Enter Player Name: ')
+      if player in players:
+        print('No duplicate names. Please try again.')
+        continue
+      break
     players.append(player)
 
   return players
@@ -38,18 +55,40 @@ def assign_roles(player_list: list[str], imposter: str) -> dict:
   return player_dict
 
 
+def reveal_roles(game_dict: dict) -> None:
+  '''Reveal roles, nba player, and hints to game players'''
+  for player in game_dict['player_list']:
+    print('\n' * 40)
+    input(f'Pass to {player} and Press Enter When Ready')
+
+    print('\n' * 40)
+    if player == game_dict['imposter']:
+      print('You are the IMPOSTER')
+      print('\nHint: ', game_dict['hint'])
+    else:
+      print('You are a NORMAL player\n')
+      print(game_dict['nba_player'])
+    
+    input('Press ENTER when done')
+  
+  print('\n' * 40)
+  print('Begin the Game')
+
+
+      
+
 
 def main():
-  player_num = int(input("Enter the Nummber of Players: "))
+  player_num = get_num_players()
   player_list = collect_players(player_num)
   imposter = choose_imposter(player_list)
   nba_player, hint = random_nba_player()
   player_roles = assign_roles(player_list, imposter)
 
   game_dict = {'player_num':player_num, 'player_list':player_list, 'imposter':imposter,'nba_player':nba_player, 'hint':hint}
-  print(game_dict)
-  
 
+  reveal_roles(game_dict)
+  
 
 
 if __name__ == "__main__":
